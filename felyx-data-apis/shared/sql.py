4511b3e5b0e10f9a155ac265d170bf115388ba68
Jonathan Robijn <jprobijn@gmail.com>
2021-02-25 09:51:33 +0100

    SImplified sql connection string function

--- felyx-data-apis/shared/sql.py
+++ felyx-data-apis/shared/sql.py
@@ -3,12 +3,5 @@ import dotenv
 
 
 def get_sql_connection_string():
-    dotenv.load_dotenv()
-
-    server = os.getenv("SQL_SERVER")
-    database = os.getenv("SQL_DATABASE")
-    username = os.getenv("SQL_USERNAME")
-    password = os.getenv("SQL_PASSWORD")
-    driver = os.getenv("SQL_DRIVER")
-    
-    return f"DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}"
+    dotenv.load_dotenv()   
+    return "DRIVER=%(SQL_DRIVER)s;SERVER=%(SQL_SERVER)s;PORT=1433;DATABASE=%(SQL_DATABASE)s;UID=%(SQL_USERNAME)s;PWD={%(SQL_PASSWORD)s}" % os.environ
