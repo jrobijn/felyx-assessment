b5585b55ebbd049685e6755a04252c20bce27023
Jonathan Robijn <jprobijn@gmail.com>
2021-02-25 10:08:47 +0100

    Added addLocation query parameter to SQL API

--- felyx-data-apis/get_reservations_in_azuresql/__init__.py
+++ felyx-data-apis/get_reservations_in_azuresql/__init__.py
@@ -3,7 +3,7 @@ import json
 import pyodbc
 from dotenv import load_dotenv
 from shared.data import reservation_schema
-from shared.utils import convert_dict_list_to_csv_string
+from shared.utils import convert_dict_list_to_csv_string, parse_boolean
 from shared.sql import get_sql_connection_string
 
 import azure.functions as func
@@ -14,11 +14,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     try:
         output_format = req.params.get("format", "json")
+        add_location = req.params.get("addLocation", "False")
 
         columns = reservation_schema.keys()
+        if parse_boolean(add_location):
+            columns += ["wgs84_polygon", "title"]
+            query = "SELECT * FROM Reservations r LEFT JOIN Locations l ON r.location_id = l.id"
+        else:
+            query = "SELECT * FROM Reservations"
+
         with pyodbc.connect(get_sql_connection_string()) as conn:
             with conn.cursor() as cursor:
-                cursor.execute("SELECT * FROM Reservations")
+                cursor.execute(query)
                 reservations = [dict(zip(columns, row)) for row in cursor.fetchall()]
 
         if output_format == "json":
